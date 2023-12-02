de25f1da0b3dae8d6509a8c3df83d2998c126562
Hao Lu <64831254+haolu253@users.noreply.github.com>
2023-12-02 13:19:46 +0700

    add slick

--- ai-innovator/js/main.js
+++ ai-innovator/js/main.js
@@ -1,3 +1,32 @@
 $(document).ready(function () {
 
+    $('.partners-slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        dots: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
 });
