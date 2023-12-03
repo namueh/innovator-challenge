203d1e4b022b24ab55c7cb621b1b913831a3fea9
Hao Lu <64831254+haolu253@users.noreply.github.com>
2023-12-03 14:05:16 +0700

    Update comple People page

--- ai-innovator/js/main.js
+++ ai-innovator/js/main.js
@@ -7,20 +7,56 @@ $(document).ready(function () {
         dots: true,
         speed: 500,
         slidesToShow: 4,
-        slidesToScroll: 1,
+        slidesToScroll: 4,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
+                    slidesToScroll: 3,
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
+                    slidesToScroll: 2,
                 }
             }
         ]
     });
+
+    $('.people-2rows-slider').slick({
+		rows: 2,
+		dots: true,
+		arrows: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+
 });
